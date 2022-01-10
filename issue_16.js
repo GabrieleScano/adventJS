41ffdcdcf1e954d41a9311e4a4a09b667da3573c
gabri <gabrielescano.bcn@gmail.com>
2022-01-10 22:39:40 +0100

    Issue 16 completed

---
+++ issue_16.js
@@ -0,0 +1,47 @@
+// Lara Eloft ha encontrado unos restos élficos en una cueva, 
+// cerca del Círculo Polar Ártico, a 8 km al norte de Rovaniemi.
+// Ahora se encuentra descifrando unas misteriosas cartas que 
+// contiene información sobre unos números que le puede hacer 
+// llegar al próximo objetivo.
+// Lara tiene un documento que contiene una serie de números 
+// que pueden ser usados para descifrarlos:
+// Lara, además, ha notado una cosa. Los símbolos se restan si 
+// están inmediatamente a la izquierda de otro mayor. 😱
+// Tenemos que crear una función que nos pasa una cadena de 
+// texto con símbolos y tenemos que transformarlo al 
+// número correcto. ¡Ojo! Si encuentras un símbolo 
+// que no entendemos, mejor que devolvamos un NaN:
+ 
+console.log(decodeNumbers('...')) // 3
+decodeNumbers('.,') // 4 (5 - 1)
+decodeNumbers(',.') // 6 (5 + 1)
+decodeNumbers(',...') // 8 (5 + 3)
+decodeNumbers('.........!') // 107 (1 + 1 + 1 + 1 + 1 + 1 + 1 - 1 + 100)
+decodeNumbers('.;') // 49 (50 - 1)
+decodeNumbers('..,') // 5 (-1 + 1 + 5)
+decodeNumbers('..,!') // 95 (1 - 1 - 5 + 100)
+decodeNumbers('.;!') // 49 (-1 -50 + 100)
+decodeNumbers('!!!') // 300
+decodeNumbers(';!') // 50
+decodeNumbers(';.W') // NaN
+
+
+const keys = { 
+    '.': 1, 
+    ',': 5, 
+    ':': 10,
+    ';': 50, 
+    '!': 100 
+  }
+  
+  const decodeNumbers = symbols =>  {
+      return symbols.split('').reduceRight( ( result, symbol, index, array ) => {
+          const currentValue = keys[symbol] || NaN
+          return currentValue < keys[array[index + 1]]
+          ? result - currentValue
+          : result + currentValue
+      }, 0)
+  }
+ 
+  
+  
